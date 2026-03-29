27942fd18ef633351fba69865f275e252b8463af
Daehee Han <gkseogml4959@gmail.com>
2026-03-29 16:24:56 +0900

    [Bronze III] Title: And Then There Was 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11520. And Then There Was 5/And Then There Was 5.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int P, D; cin >> P >> D;
+  cout << D << " " << 5 << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int N; cin >> N;
+  while (N--) {
+    solve();
+  }
+}
